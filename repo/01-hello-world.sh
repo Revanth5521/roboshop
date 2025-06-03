d24c58dc37a7cbf285a1fe02ef9ea3a19fcd7625
Revanth <revanthch01@gmail.com>
2025-06-03 06:50:17 +0530

    hello world started

--- repo/01-hello-world.sh
+++ repo/01-hello-world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "hello world"
+
+# Get CPU idle percentage and calculate utilization
+cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+cpu_usage=$(echo "100 - $cpu_idle" | bc)
+
+echo "CPU Utilization: $cpu_usage %"
